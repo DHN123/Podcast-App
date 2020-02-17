230cff6b0580b4f18b5b05a5e831f36cf63363ab
nhido <nhido119966@gmail.com>
2020-02-17 10:26:09 +0700

    add react-navigation and setup basic tab nav

--- podcast/App.tsx
+++ podcast/App.tsx
@@ -11,14 +11,15 @@
 import React from 'react';
 import { Box, UtilityThemeProvider } from 'react-native-design-utility';
 import { theme } from './src/constants/theme'
+import MainNavigation from './src/navigations/MainNavigation';
 
 // declare var global: {HermesInternal: null | {}}; 
 
 const App = () => {
   return (
     <UtilityThemeProvider theme={ theme }>
-        <Box bg='blueDark' f={ 1 }>
-
+        <Box f={ 1 }>
+          <MainNavigation/>
         </Box>
     </UtilityThemeProvider>
   );
